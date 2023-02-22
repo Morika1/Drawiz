32aefe6b4f95da97c25bc5101b319478825a4b30
Morika1 <68543807+Morika1@users.noreply.github.com>
2023-02-22 20:18:04 +0200

    removed comments

--- app/src/main/java/com/example/drawiz/activities/LobbyActivity.java
+++ app/src/main/java/com/example/drawiz/activities/LobbyActivity.java
@@ -39,42 +39,31 @@ public class LobbyActivity extends AppCompatActivity {
     private Words_Fragment words_fragment;
     private ArrayList<Room> allRooms = new ArrayList<Room>();
     Adapter_Room adapter_room;
-    //  Lobby lobby;
     UserInfo user;
 
     ValueEventListener roomsEventListener = new ValueEventListener() {
         @Override
         public void onDataChange(@NonNull DataSnapshot snapshot) {
             if(snapshot.exists()){
-                //  HashMap<String,HashMap<String, Object>> map= (HashMap<String,HashMap<String, Object>>)snapshot.getValue();
                 for(DataSnapshot roomSnapshot: snapshot.getChildren() ) {
                    // if(roomSnapshot.exists()){
                         HashMap<String, Object> roomData = (HashMap<String, Object>)roomSnapshot.getValue();
-    //                    for(int i=0; i<roomData.size(); i++){
-    //                        Log.d("Value",(String) roomData.get("artist"));
-    //                    }
+
                     if (roomData.containsKey("artist")){
                         Room aRoom = new Room().
                                 setRoomId((String)roomSnapshot.getKey()).
                                 setArtist(new Gson().fromJson((String) roomData.get("artist").toString(), UserInfo.class)).
-                                // setGuesser(new Gson().fromJson((String) roomData.get("guesser").toString(), UserInfo.class)).
                                         setWord(new Gson().fromJson((String) roomData.get("word").toString(), Word.class));
-                              //  setTheGame(new Gson().fromJson((String) roomData.get("game").toString(), Game.class));
 
                         if (!aRoom.getRoomId().equals(user.getUserId()))
                             allRooms.add(aRoom);
 
                     }
-                  //  }
 
-                    }
-               // Log.d("allrooms size", allRooms.size()+"\n" );
-
-                // lobby.setRooms(allRooms);
+                }
+              
             }
-//                    lobby.setRooms((HashMap<String,Room>) snapshot.getValue());
-//
-            //adapter_room = new Adapter_Room(LobbyActivity.this, lobby.getRoomsList());
+
             adapter_room = new Adapter_Room(LobbyActivity.this, allRooms);
             adapter_room.setCallBack_openPageProtocol(callBack_openPageProtocol);
             lobby_LST_rooms.setLayoutManager(new LinearLayoutManager(LobbyActivity.this));
@@ -96,7 +85,7 @@ public class LobbyActivity extends AppCompatActivity {
             FirebaseDatabase db = FirebaseDatabase.getInstance();
             DatabaseReference ref= db.getReference("lobby");
 
-            removeListener(ref.child("rooms"),roomsEventListener); //TODO pass ref and listener
+            removeListener(ref.child("rooms"),roomsEventListener); 
             user.setMode(UserInfo.MODE.GUESSER);
             room.setGuesser(user);
             saveGuesserToDB(room);
@@ -109,9 +98,8 @@ public class LobbyActivity extends AppCompatActivity {
         }
     };
 
-    private void removeListener(DatabaseReference ref, ValueEventListener listener) { //TODO update method to get ref and listener to remove
-//        FirebaseDatabase db = FirebaseDatabase.getInstance();
-//        DatabaseReference ref= db.getReference("lobby");
+    private void removeListener(DatabaseReference ref, ValueEventListener listener) { 
+
 
         ref.removeEventListener(listener);
     }
@@ -120,15 +108,12 @@ public class LobbyActivity extends AppCompatActivity {
     private void saveGuesserToDB(Room room) {
         FirebaseDatabase db = FirebaseDatabase.getInstance();
         DatabaseReference ref= db.getReference("lobby");
-       // HashMap<String, Object> guesserjson = new HashMap<>();
-      //  guesserjson.put("guesser", new Gson().toJson(room.getGuesser()));
+
         ref.child("rooms").
                 child(room.getRoomId()).
                 child("guesser").
                 setValue(new Gson().toJson(room.getGuesser()));
-                //updateChildren(guesserjson);
-
-
+  
 
 
     }
@@ -157,11 +142,8 @@ public class LobbyActivity extends AppCompatActivity {
                 DatabaseReference ref= db.getReference("lobby");
                 if(snapshot.exists()){
                     if(!snapshot.getValue().equals("empty")){
-                       // openGame(ref.child("rooms").child(room.getRoomId()).child("guesser"), room);
                         Intent intent = new Intent(LobbyActivity.this, GameActivity.class);
                         intent.putExtra(GameActivity.KEY_ROOM, room);
-                      //  removeListener(ref.child("rooms").child(room.getRoomId()).child("guesser"), guesserEventListener); // TODO add ref and listener to signiture
-                      //  ref.child("rooms").child(room.getRoomId()).child("guesser").removeEventListener();
                         startActivity(intent);
                         finish();
 
@@ -179,29 +161,6 @@ public class LobbyActivity extends AppCompatActivity {
         FirebaseDatabase db = FirebaseDatabase.getInstance();
         DatabaseReference ref= db.getReference("lobby");
         ref.child("rooms").child(room.getRoomId()).child("guesser").addValueEventListener(guesserEventListener);
-//        ref.child("rooms").child(room.getRoomId()).child("guesser").addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
-//
-//                if(snapshot.exists()){
-//                    if(!snapshot.getValue().equals("empty")){
-//                        Intent intent = new Intent(LobbyActivity.this, GameActivity.class);
-//                        intent.putExtra(GameActivity.KEY_ROOM, room);
-//                        ref.child("rooms").child(room.getRoomId()).child("guesser").removeEventListener();
-//                        startActivity(intent);
-//                        finish();
-//
-//                    }
-//                }
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//                //handle error
-//
-//            }
-//        });
-
 
     }
 
@@ -217,18 +176,11 @@ public class LobbyActivity extends AppCompatActivity {
         Intent prevIntent = getIntent();
         user =((UserInfo) prevIntent.getExtras().getSerializable(HomeActivity.KEY_USER));
 
-        //  lobby = new Lobby();
+    
         words_fragment = new Words_Fragment();
         words_fragment.setCallBack_wordProtocol(callBack_wordProtocol);
-        //     words_fragment.setCallBack_readyProtocol(callBack_readyProtocol);
-
-//        allRooms = new ArrayList<Room>();
+    
         readRoomsFromDB();
-//        Lobby lobby = new Lobby().setRooms(allRooms);
-//
-//        adapter_room = new Adapter_Room(this, lobby.getRoomsList());
-//        lobby_LST_rooms.setLayoutManager(new LinearLayoutManager(this));
-//        lobby_LST_rooms.setAdapter(adapter_room);
 
         getSupportFragmentManager().beginTransaction().add(R.id.lobby_LAY_words, words_fragment).commit();
     }
@@ -257,15 +209,13 @@ public class LobbyActivity extends AppCompatActivity {
 
 
     private void saveRoomToDB(Room room){
-        // lobby.getRooms().put(room.getRoomNumber(),room);
-       // String roomJson = new Gson().toJson(room);
+ 
         Gson json= new Gson();
         HashMap<String, Object> roomJson = new HashMap<>();
         roomJson.put("roomId", room.getRoomId());
         roomJson.put("artist", json.toJson(room.getArtist()));
         roomJson.put("guesser", "empty");
         roomJson.put("word", json.toJson(room.getWord()));
-       // roomJson.put("game", json.toJson(room.getTheGame()));
         FirebaseDatabase db = FirebaseDatabase.getInstance();
         DatabaseReference ref= db.getReference("lobby");
         ref.child("rooms").child(room.getRoomId()).setValue(roomJson);
@@ -273,50 +223,10 @@ public class LobbyActivity extends AppCompatActivity {
 
 
     private void readRoomsFromDB(){
-        //allRooms = new ArrayList<Room>();
         FirebaseDatabase db = FirebaseDatabase.getInstance();
         DatabaseReference ref= db.getReference("lobby");
 
         ref.child("rooms").addValueEventListener(roomsEventListener);
-//        ref.child("rooms").addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                if(snapshot.exists()){
-//                  //  HashMap<String,HashMap<String, Object>> map= (HashMap<String,HashMap<String, Object>>)snapshot.getValue();
-//                    for(DataSnapshot roomSnapshot: snapshot.getChildren() ) {
-//                        HashMap<String, Object> roomData = (HashMap<String, Object>)roomSnapshot.getValue();
-//                        for(int i=0; i<roomData.size(); i++){
-//                            Log.d("Value",(String) roomData.get("artist"));
-//                        }
-//                        Room aRoom = new Room().
-//                                setRoomId((String)roomSnapshot.getKey()).
-//                                setArtist(new Gson().fromJson((String) roomData.get("artist").toString(), UserInfo.class)).
-//                               // setGuesser(new Gson().fromJson((String) roomData.get("guesser").toString(), UserInfo.class)).
-//                                setWord(new Gson().fromJson((String) roomData.get("word").toString(), Word.class)).
-//                                setTheGame(new Gson().fromJson((String) roomData.get("game").toString(), Game.class));
-//
-//                        if (!aRoom.getRoomId().equals(user.getUserId()))
-//                            allRooms.add(aRoom);
-//                    }
-//                    Log.d("allrooms size", allRooms.size()+"\n" );
-//
-//                    // lobby.setRooms(allRooms);
-//                }
-////                    lobby.setRooms((HashMap<String,Room>) snapshot.getValue());
-////
-//                //adapter_room = new Adapter_Room(LobbyActivity.this, lobby.getRoomsList());
-//                adapter_room = new Adapter_Room(LobbyActivity.this, allRooms);
-//                adapter_room.setCallBack_openPageProtocol(callBack_openPageProtocol);
-//                lobby_LST_rooms.setLayoutManager(new LinearLayoutManager(LobbyActivity.this));
-//                lobby_LST_rooms.setAdapter(adapter_room);
-//
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//                // handle error
-//            }
-//        });
     }
 
     private void findViews() {
@@ -325,170 +235,4 @@ public class LobbyActivity extends AppCompatActivity {
         lobby_BTN_creategame = findViewById(R.id.lobby_BTN_creategame);
         lobby_BTN_back = findViewById(R.id.lobby_BTN_back);
     }
-
-//    private AppCompatImageView lobby_IMG_background;
-//    private MaterialButton lobby_BTN_creategame;
-//    private RecyclerView lobby_LST_rooms;
-//    private Words_Fragment words_fragment;
-//    private ArrayList<Room> allRooms;
-//    Adapter_Room adapter_room;
-//    Lobby lobby;
-//    UserInfo user;
-//
-//
-//    CallBack_openPageProtocol callBack_openPageProtocol = new CallBack_openPageProtocol() {
-//        @Override
-//        public void openGame(Room room) { // user joining game as guesser (user2)
-//            user.setMode(UserInfo.MODE.GUESSER);
-//            room.setGuesser(user);
-//            saveGuesserToFireBase(room);
-//            Intent intent = new Intent(LobbyActivity.this, GameActivity.class);
-//            intent.putExtra(GameActivity.KEY_ROOM,room );
-//            startActivity(intent);
-//            finish();
-//
-//
-//        }
-//    };
-//
-//
-//
-//    private void saveGuesserToFireBase(Room room) {
-//        FirebaseDatabase db = FirebaseDatabase.getInstance();
-//        DatabaseReference ref= db.getReference("lobby");
-//        ref.child("rooms").child(""+room.getRoomNumber()).child("guesser").setValue(room.getGuesser());
-//
-//    }
-//
-//    CallBack_wordProtocol callBack_wordProtocol = new CallBack_wordProtocol() {
-//        @Override
-//        public void addWord(Word word) {
-//            Room room = new Room().
-//                    setRoomNumber(Integer.parseInt(lobby.getRoomNumber())).
-//                    setArtist(user).setWord(word);
-//
-//            saveRoomToDB(room);
-//            words_fragment.turnWords(); // turn off
-//            lobby_BTN_creategame.setVisibility(View.INVISIBLE);
-//            words_fragment.startWaiting(); // start waiting lottie animation
-//            readGuesserFromDB(room);
-//
-//
-//
-//
-//
-//        }
-//    };
-//
-//    private void readGuesserFromDB(Room room) {
-//
-//        FirebaseDatabase db = FirebaseDatabase.getInstance();
-//        DatabaseReference ref= db.getReference("lobby");
-//        ref.child("rooms").child(room.getRoomNumber()+"").child("guesser").addListenerForSingleValueEvent(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
-//
-//                if(snapshot.exists()){
-//                    Intent intent = new Intent(LobbyActivity.this, GameActivity.class);
-//                    startActivity(intent);
-//                    finish();
-//
-//                }
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//                //handle error
-//
-//            }
-//        });
-//
-//
-//    }
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_lobby);
-//
-//        findViews();
-//        initViews();
-//        MyImageUtils.getInstance().loadUri("https://www.leatheredgepaint.com/hs-fs/hubfs/social-suggested-images/Custom_Color_Service.jpg?width=1440&name=Custom_Color_Service.jpg", lobby_IMG_background);
-//
-//        Intent prevIntent = getIntent();
-//        user =((UserInfo) prevIntent.getExtras().getSerializable(HomeActivity.KEY_USER));
-//
-//        lobby = new Lobby();
-//        words_fragment = new Words_Fragment();
-//        words_fragment.setCallBack_wordProtocol(callBack_wordProtocol);
-//   //     words_fragment.setCallBack_readyProtocol(callBack_readyProtocol);
-//
-////        allRooms = new ArrayList<Room>();
-//        readRoomsFromDB();
-////        Lobby lobby = new Lobby().setRooms(allRooms);
-////
-////        adapter_room = new Adapter_Room(this, lobby.getRoomsList());
-////        lobby_LST_rooms.setLayoutManager(new LinearLayoutManager(this));
-////        lobby_LST_rooms.setAdapter(adapter_room);
-//
-//        getSupportFragmentManager().beginTransaction().add(R.id.lobby_LAY_words, words_fragment).commit();
-//    }
-//
-//    private void initViews() {
-//        lobby_BTN_creategame.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//               user.setMode(UserInfo.MODE.ARTIST);
-//               words_fragment.turnWords(); // turn on words
-//            }
-//        });
-//
-//    }
-//
-//
-//    private void saveRoomToDB(Room room){
-//        lobby.getRooms().put(room.getRoomNumber(),room);
-//
-//        FirebaseDatabase db = FirebaseDatabase.getInstance();
-//        DatabaseReference ref= db.getReference("lobby");
-//        ref.child("rooms").child(room.getRoomNumber()).setValue(room);
-//    }
-//
-//
-//    private void readRoomsFromDB(){
-//        //allRooms = new ArrayList<Room>();
-//        FirebaseDatabase db = FirebaseDatabase.getInstance();
-//        DatabaseReference ref= db.getReference("lobby");
-//        ref.child("rooms").addListenerForSingleValueEvent(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                if(snapshot.exists()){
-//                    allRooms = (ArrayList<Room>) snapshot.getValue();
-//                    Log.d("allrooms size", allRooms.size()+"" );
-//                   // lobby.setRooms(allRooms);
-//
-//
-//                }
-////                    lobby.setRooms((HashMap<String,Room>) snapshot.getValue());
-////
-//                //adapter_room = new Adapter_Room(LobbyActivity.this, lobby.getRoomsList());
-//                adapter_room = new Adapter_Room(LobbyActivity.this, allRooms);
-//                adapter_room.setCallBack_openPageProtocol(callBack_openPageProtocol);
-//                lobby_LST_rooms.setLayoutManager(new LinearLayoutManager(LobbyActivity.this));
-//                lobby_LST_rooms.setAdapter(adapter_room);
-//
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//                // handle error
-//            }
-//        });
-//    }
-//
-//    private void findViews() {
-//        lobby_LST_rooms = findViewById(R.id.lobby_LST_rooms);
-//        lobby_IMG_background = findViewById(R.id.lobby_IMG_background);
-//        lobby_BTN_creategame = findViewById(R.id.lobby_BTN_creategame);
-//    }
 }
